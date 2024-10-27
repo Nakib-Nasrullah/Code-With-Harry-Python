1ae7770adc2d48f99ad15225588b198de3c19f69
Binary Vain <95162026+Nakib-Nasrullah@users.noreply.github.com>
2024-10-27 17:08:04 +0600

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+dict1 = {"Name": "Nakib", "Age": 27, "Ocup": "Student"}
+print(dict1)
+dict2 = {"Name":"Abdullah", "Age": 23, "Ocup":"Student"}
+print(dict2)
---
+++ implicitTypeCasting.py
@@ -0,0 +1,4 @@
+#implicit TypeCasting
+c = 2.5
+d = 10
+print("Sum is =", c + d)

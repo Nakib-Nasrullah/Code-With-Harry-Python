de3f8f53a6bf1e27d5e52a0a227e1488be4561b7
Binary Vain <95162026+Nakib-Nasrullah@users.noreply.github.com>
2024-10-27 20:51:18 +0600

    Add files via upload

---
+++ userInputMath.py
@@ -0,0 +1,7 @@
+x = float(input("Enter first number :"))
+y = float(input("Enter second Number :"))
+print("sum is =",x + y)
+print("Sub is =",x - y)
+print("div is =",x / y)
+print("multi is =",x * y)
+print("Mod is =",x % y)
